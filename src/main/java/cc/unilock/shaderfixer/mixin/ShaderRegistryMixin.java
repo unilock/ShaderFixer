d4e4c4c6f1c995794d478e709bca47faef851d82
unilock <unilock@fennet.rentals>
2024-06-08 13:03:42 -0400

    fix
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/shaderfixer/mixin/ShaderRegistryMixin.java
+++ src/main/java/cc/unilock/shaderfixer/mixin/ShaderRegistryMixin.java
@@ -1,25 +1,27 @@
 package cc.unilock.shaderfixer.mixin;
 
+import Reika.DragonAPI.IO.Shaders.ShaderProgram;
 import Reika.DragonAPI.IO.Shaders.ShaderRegistry;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = ShaderRegistry.class, remap = false)
 public class ShaderRegistryMixin {
-    @Redirect(method = "createShader(LReika/DragonAPI/Base/DragonAPIMod;Ljava/lang/String;Ljava/lang/Class;Ljava/lang/String;LReika/DragonAPI/IO/Shaders/ShaderRegistry$ShaderDomain;)LReika/DragonAPI/IO/Shaders/ShaderProgram;", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/OpenGlHelper;shadersSupported:Z", opcode = Opcodes.GETSTATIC))
-    private static boolean createShader$getShadersSupported() {
-        return false;
+    @Inject(method = "createShader(LReika/DragonAPI/Base/DragonAPIMod;Ljava/lang/String;Ljava/lang/Class;Ljava/lang/String;LReika/DragonAPI/IO/Shaders/ShaderRegistry$ShaderDomain;)LReika/DragonAPI/IO/Shaders/ShaderProgram;", at = @At("HEAD"), cancellable = true)
+    private static void createShader(CallbackInfoReturnable<ShaderProgram> cir) {
+        cir.setReturnValue(null);
     }
 
-    @Redirect(method = "runShader(LReika/DragonAPI/IO/Shaders/ShaderProgram;)Z", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/OpenGlHelper;shadersSupported:Z", opcode = Opcodes.GETSTATIC))
-    private static boolean runShader$getShadersSupported() {
-        return false;
+    @Inject(method = "runShader(LReika/DragonAPI/IO/Shaders/ShaderProgram;)Z", at = @At(value = "HEAD"), cancellable = true)
+    private static void runShader(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
     }
 
-    @Redirect(method = "completeShader()V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/renderer/OpenGlHelper;shadersSupported:Z", opcode = Opcodes.GETSTATIC))
-    private static boolean completeShader$getShadersSupported() {
-        return false;
+    @Inject(method = "completeShader()V", at = @At(value = "HEAD"), cancellable = true)
+    private static void completeShader(CallbackInfo ci) {
+        ci.cancel();
     }
 }
